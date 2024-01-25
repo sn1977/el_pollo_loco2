ebf74ae965988ffb1ca7030e24dacd47615a37ca
Sascha Nemeth <sascha.nemeth@me.com>
2024-01-25 15:08:22 +0100

    ad jsdoc comments in chicken.class.js

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,18 +1,50 @@
+/**
+ * Represents a chicken enemy in the game.
+ * @extends MovableObject
+ */
 class Chicken extends MovableObject {
-
+    /**
+     * Y-coordinate of the chicken.
+     * @type {number}
+     */
     y = 350;
+
+    /**
+     * Height of the chicken.
+     * @type {number}
+     */
     height = 80;
+    /**
+     * Width of the chicken.
+     * @type {number}
+     */
     width = 80;
+
+    /**
+     * Flag indicating whether the chicken is dead.
+     * @type {boolean}
+     */
     isDead = false;
 
+    /**
+     * Array of images for the walking animation.
+     * @type {string[]}
+     */
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
 
+    /**
+     * Array of images for the dead animation.
+     * @type {string[]}
+     */
     IMAGES_DEAD = ['img/3_enemies_chicken/chicken_normal/2_dead/dead.png'];
 
+    /**
+     * Constructs a new Chicken instance with initial settings.
+     */
     constructor() {
         super().loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
@@ -22,12 +54,13 @@ class Chicken extends MovableObject {
         this.animate();
     }
 
+    /**
+     * Initiates the chicken's animations.
+     */
     animate() {
         setStoppableInterval(() => {
             this.moveLeft();
         }, 1000 / 60);
-        // addInterval(movementIntervalId);
-
 
         setStoppableInterval(() => {
             if (this.isDead) {
@@ -36,6 +69,5 @@ class Chicken extends MovableObject {
                 this.playAnimation(this.IMAGES_WALKING);
             }
         }, 200);
-        // addInterval(animationIntervalId);
     }
 }
