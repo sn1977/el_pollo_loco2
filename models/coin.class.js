276776886cc3f7351ce6cc12cab51051cd972a3a
Sascha Nemeth <sascha.nemeth@me.com>
2024-01-25 15:23:15 +0100

    ad jsdoc comments in coin.class.js

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,9 +1,21 @@
+/**
+ * Represents a coin object in the game.
+ * @extends MovableObject
+ */
 class Coin extends MovableObject {
+    /**
+     * Array of paths to the coin images for animation.
+     * @type {string[]}
+     */
     IMAGES_COIN = [
         'img/8_coin/coin_1.png',
         'img/8_coin/coin_2.png'
     ];
 
+    /**
+     * Object containing the offset values for collision detection.
+     * @type {{top: number, left: number, right: number, bottom: number}}
+     */
     offset = {
         top: 55,
         left: 50,
@@ -11,6 +23,9 @@ class Coin extends MovableObject {
         bottom: 55
     };
 
+    /**
+     * Constructs a new Coin instance and its random position.
+     */
     constructor() {
         super().loadImage(this.IMAGES_COIN[0]);
         this.loadImages(this.IMAGES_COIN);
@@ -19,10 +34,12 @@ class Coin extends MovableObject {
         this.animate();
     }
 
+    /**
+     * Initiates the coin's animation.
+     */
     animate() {
         setStoppableInterval(() => {
             this.playAnimation(this.IMAGES_COIN);
         }, 200);
-        // addInterval(intervalId);
     }
 }
