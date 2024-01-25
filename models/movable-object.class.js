6bf0d87b0430e61f72bae9d1625bb97a9a1d8582
Sascha Nemeth <sascha.nemeth@me.com>
2024-01-25 15:52:44 +0100

    ad jsdoc comments in movable-object.class.js

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,18 @@
+/**
+ * Represents a movable object in the game.
+ * Extends the DrawableObject class.
+ */
 class MovableObject extends DrawableObject {
-    speed = 0.15;
-    otherDirection = false;
-    speedY = 0;
-    acceleration = 2.5;
-    energy = 100;
-    lastHit = 0;
+    speed = 0.15;            // Horizontal movement speed
+    otherDirection = false;  // Indicates if the object is facing the opposite direction
+    speedY = 0;             // Vertical speed (for jumping/falling)
+    acceleration = 2.5;     // Acceleration due to gravity
+    energy = 100;           // Energy level of the object
+    lastHit = 0;            // Timestamp of the last hit taken by the object
 
+    /**
+     * Applies gravity to the movable object, making it fall when above ground.
+     */
     applyGravity() {
         setStoppableInterval(() => {
             if (this.isAboveGround() || this.speedY > 0) {
@@ -13,9 +20,12 @@ class MovableObject extends DrawableObject {
                 this.speedY -= this.acceleration;
             }
         }, 1000 / 25);
-        // addInterval(intervalId);
     }
 
+    /**
+     * Checks if the object is above the ground.
+     * @returns {boolean} - True if above ground, false otherwise.
+     */
     isAboveGround() {
         if (this instanceof ThrowableObject) { // Throwable object should always fall!!!
             return true;
@@ -24,14 +34,24 @@ class MovableObject extends DrawableObject {
         }
     }
 
+    /**
+     * Moves the object to the right.
+     */
     moveRight() {
         this.x += this.speed;
     }
 
+    /**
+     * Moves the object to the left.
+     */
     moveLeft() {
         this.x -= this.speed;
     }
 
+    /**
+     * Plays the animation for the movable object.
+     * @param {array} images - An array of image paths for the animation.
+     */
     playAnimation(images) {
         let i = this.currentImage % images.length;
         let path = images[i];
@@ -39,14 +59,26 @@ class MovableObject extends DrawableObject {
         this.currentImage++;
     }
 
+    /**
+     * Makes the object jump by applying a vertical speed.
+     */
     jump() {
         this.speedY = 30;
     }
 
-    isFalling(){
+    /**
+     * Checks if the object is falling.
+     * @returns {boolean} - True if falling, false otherwise.
+     */
+    isFalling() {
         return this.speedY < 0;
     }
 
+    /**
+     * Checks if the object is colliding with another movable object.
+     * @param {MovableObject} mo - The other movable object to check for collision with.
+     * @returns {boolean} - True if colliding, false otherwise.
+     */
     isColliding(mo) {
         return this.x + this.width - this.offset.right > mo.x + mo.offset.left &&
             this.y + this.height - this.offset.bottom > mo.y + mo.offset.top &&
@@ -54,6 +86,10 @@ class MovableObject extends DrawableObject {
             this.y + this.offset.top < mo.y + mo.height - mo.offset.bottom;
     }
 
+    /**
+     * Decreases the energy of the object when hit.
+     * If energy reaches 0, the object is considered dead.
+     */
     hit() {
         this.energy -= 5;
         if (this.energy < 0) {
@@ -63,10 +99,18 @@ class MovableObject extends DrawableObject {
         }
     }
 
+    /**
+     * Checks if the object is dead (energy is 0).
+     * @returns {boolean} - True if dead, false otherwise.
+     */
     isDead() {
         return this.energy === 0;
     }
 
+    /**
+     * Checks if the object is hurt based on the time since the last hit.
+     * @returns {boolean} - True if hurt, false otherwise.
+     */
     isHurt() {
         let timepassed = new Date().getTime() - this.lastHit;
         timepassed = timepassed / 1000;
