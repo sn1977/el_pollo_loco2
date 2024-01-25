9ba8184d9896a7521f743ec7b29b13d71c454195
Sascha Nemeth <sascha.nemeth@me.com>
2024-01-25 14:21:16 +0100

    ad jsdoc comments in background-object.class.js

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,9 +1,26 @@
+/**
+ * Represents a background object in the game.
+ * Background objects are used to create the visual environment of the game.
+ */
 class BackgroundObject extends MovableObject {
+    /**
+     * The width of the background object.
+     */
     width = 720;
+
+    /**
+     * The height of the background object.
+     */
     height = 480;
+
+    /**
+     * Constructs a new BackgroundObject.
+     * @param {string} imagePath - The path to the image file for this background object.
+     * @param {number} x - The x-coordinate where the background object will be placed.
+     */
     constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.x = x;
-        this.y = 480 - this.height;
+        this.y = 480 - this.height; // Position at the bottom of the canvas
     }
 }
