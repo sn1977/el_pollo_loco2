5bb1407b4b56cf243276e9fb17e9adfec34e8a79
Sascha Nemeth <sascha.nemeth@me.com>
2024-01-25 16:01:39 +0100

    ad jsdoc comments in status-bar-coin.class.js

--- models/status-bar-coin.class.js
+++ models/status-bar-coin.class.js
@@ -1,4 +1,14 @@
+/**
+ * Represents a status bar for collecting coins in the game.
+ * @extends DrawableObject
+ */
 class StatusBarCoin extends DrawableObject {
+
+    /**
+     * An array of image paths representing different coin fill levels for the status bar.
+     * The images indicate the coin fill percentage in 20% increments.
+     * @type {string[]}
+     */
     IMAGES_COIN = [
         'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png',
         'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png',
@@ -7,8 +17,16 @@ class StatusBarCoin extends DrawableObject {
         'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png',
         'img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png'
     ];
+
+    /**
+     * The current amount of coins in the status bar.
+     * @type {number}
+     */
     amountCoins = 0;
 
+    /**
+     * Constructs a new StatusBarCoin instance.
+     */
     constructor() {
         super();
         this.loadImages(this.IMAGES_COIN);
@@ -19,16 +37,27 @@ class StatusBarCoin extends DrawableObject {
         this.width = 220;
     }
 
+    /**
+     * Sets the current amount of coins in the status bar and updates the displayed image.
+     * @param {number} amountCoins - The current amount of coins to set.
+     */
     setAmountCoins(amountCoins) {
         this.amountCoins = amountCoins;
         let imagePath = this.IMAGES_COIN[this.resolveImagesCoinIndex()];
         this.img = this.imageCache[imagePath];
     }
 
+    /**
+     * Increments the current amount of coins in the status bar by 1.
+     */
     collectCoins() {
         this.amountCoins += 1;
     }
 
+    /**
+     * Resolves the index of the image to use based on the current amount of coins.
+     * @returns {number} - The index of the image to use.
+     */
     resolveImagesCoinIndex() {
         if (this.amountCoins >= 10) {
             return 5;
