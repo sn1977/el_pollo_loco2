a93adf2254733a7156df9d6a464a1949fa439a51
Sascha Nemeth <sascha.nemeth@me.com>
2024-01-25 13:58:31 +0100

    ad jsdoc comments in level1.js & structuring code

--- levels/level1.js
+++ levels/level1.js
@@ -9,113 +9,101 @@ const layers = [
     '1_first_layer/1.png'
 ];
 
-const backgroundObjects = [];
-const repeats = 4; // Wie oft das gesamte Muster wiederholt werden soll
-const xIncrement = 719; // Verschiebung auf der x-Achse für jede Wiederholung
+const repeats = 4; // How many times to repeat the entire pattern
+const xIncrement = 719; // Displacement on the x-axis for each repetition
 
-// for (let repeat = 0; repeat < repeats; repeat++) {
-//     for (let i = 0; i < layers.length; i++) {
-//         const xPosition = repeat * xIncrement * 2 - xIncrement + (i >= layers.length / 2 ? xIncrement : 0);
-//         const imagePath = `img/5_background/layers/${layers[i]}`;
-//         backgroundObjects.push(new BackgroundObject(imagePath, xPosition));
-//     }
-// }
-// const chickens = [];
-// for (let i = 0; i < 12; i++) {
-//     chickens.push(new Chicken());
-// }
-//
-// const chickenSmall = [];
-// for (let i = 0; i < 12; i++) {
-//     chickenSmall.push(new ChickenSmall());
-// }
-//
-// // const enemies = [...chickens, ...chickenSmall, new Endboss()];
-//
-//
-// const clouds = [];
-// const totalClouds = 15;
-// const xSpacing = 500; // Abstand zwischen den Wolken
-//
-// for (let i = 0; i < totalClouds; i++) {
-//     let xPosition = i * xSpacing;
-//     clouds.push(new Cloud(xPosition));
-// }
-//
-// const coins = [];
-// for (let i = 0; i < 20; i++) {
-//     coins.push(new Coin());
-// }
-//
-// const bottles = [];
-// for (let i = 0; i < 20; i++) {
-//     bottles.push(new Bottle());
-// }
-//
-// // const level1 = new Level(
-// //     enemies,
-// //     clouds,
-// //     backgroundObjects,
-// //     coins,
-// //     bottles
-// // );
-//
-// function createLevel1(world) {
-//     const endboss = new Endboss(world); // Erstellen des Endbosses mit der World-Referenz
-//
-//     const enemies = [...chickens, ...chickenSmall, endboss]; // ...chickens, ...chickenSmall,
-//
-//     return new Level(
-//         enemies,
-//         clouds,
-//         backgroundObjects,
-//         coins,
-//         bottles
-//     );
-// }
-
+/**
+ * This function creates level 1 of the game.
+ * @param {object} world - The game world context.
+ * @returns {Level} A new level object.
+ */
 function createLevel1(world) {
-    // Erstellen Sie neue Instanzen der Hühner, kleinen Hühner, Wolken, Münzen und Flaschen
-    const chickens = [];
-    for (let i = 0; i < 12; i++) {
-        chickens.push(new Chicken());
-    }
-
-    const chickenSmall = [];
-    for (let i = 0; i < 12; i++) {
-        chickenSmall.push(new ChickenSmall());
-    }
-
-    const clouds = [];
-    const totalClouds = 15;
-    const xSpacing = 500;
-    for (let i = 0; i < totalClouds; i++) {
-        let xPosition = i * xSpacing;
-        clouds.push(new Cloud(xPosition));
-    }
-
-    const coins = [];
-    for (let i = 0; i < 20; i++) {
-        coins.push(new Coin());
-    }
-
-    const bottles = [];
-    for (let i = 0; i < 20; i++) {
-        bottles.push(new Bottle());
-    }
-
+    const chickens = createChickens(12);
+    const chickenSmall = createSmallChickens(12);
+    const clouds = createClouds(15, 500);
+    const coins = createCoins(20);
+    const bottles = createBottles(20);
     const endboss = new Endboss(world);
-    const enemies = [...chickens, ...chickenSmall,endboss];
-
-    // Zurücksetzen der Hintergrundobjekte könnte auch notwendig sein
+    const enemies = [...chickens, ...chickenSmall, endboss];
     const backgroundObjects = createBackgroundObjects();
 
     return new Level(enemies, clouds, backgroundObjects, coins, bottles);
 }
 
-// Diese Funktion erstellt die Hintergrundobjekte neu
+/**
+ * This function creates a specified number of chicken enemies.
+ * @param {number} count - The number of chickens to create.
+ * @returns {Array} An array of Chicken objects.
+ */
+function createChickens(count) {
+    const chickens = [];
+    for (let i = 0; i < count; i++) {
+        chickens.push(new Chicken());
+    }
+    return chickens;
+}
+
+/**
+ * This function creates a specified number of small chicken enemies.
+ * @param {number} count - The number of small chickens to create.
+ * @returns {Array} An array of ChickenSmall objects.
+ */
+function createSmallChickens(count) {
+    const chickenSmall = [];
+    for (let i = 0; i < count; i++) {
+        chickenSmall.push(new ChickenSmall());
+    }
+    return chickenSmall;
+}
+
+/**
+ * This function creates clouds for the background.
+ * @param {number} count - The number of clouds to create.
+ * @param {number} spacing - Spacing between each cloud.
+ * @returns {Array} An array of Cloud objects.
+ */
+function createClouds(count, spacing) {
+    const clouds = [];
+    for (let i = 0; i < count; i++) {
+        let xPosition = i * spacing;
+        clouds.push(new Cloud(xPosition));
+    }
+    return clouds;
+}
+
+/**
+ * This function creates a specified number of coins.
+ * @param {number} count - The number of coins to create.
+ * @returns {Array} An array of Coin objects.
+ */
+function createCoins(count) {
+    const coins = [];
+    for (let i = 0; i < count; i++) {
+        coins.push(new Coin());
+    }
+    return coins;
+}
+
+/**
+ * This function creates a specified number of bottles.
+ * @param {number} count - The number of bottles to create.
+ * @returns {Array} An array of Bottle objects.
+ */
+function createBottles(count) {
+    const bottles = [];
+    for (let i = 0; i < count; i++) {
+        bottles.push(new Bottle());
+    }
+    return bottles;
+}
+
+/**
+ * This function creates the background objects for the game level.
+ * @returns {Array} An array of background objects.
+ */
 function createBackgroundObjects() {
     const backgroundObjects = [];
+
     for (let repeat = 0; repeat < repeats; repeat++) {
         for (let i = 0; i < layers.length; i++) {
             const xPosition = repeat * xIncrement * 2 - xIncrement + (i >= layers.length / 2 ? xIncrement : 0);
