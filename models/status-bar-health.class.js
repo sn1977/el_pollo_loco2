f4c987dffa8c6fc30c730553afd81d97aef262e7
Sascha Nemeth <sascha.nemeth@me.com>
2024-01-25 16:07:41 +0100

    ad jsdoc comments in status-bar-health.class.js

--- models/status-bar-health.class.js
+++ models/status-bar-health.class.js
@@ -1,4 +1,14 @@
+/**
+ * Represents a status bar for displaying the player's health in the game.
+ * @extends DrawableObject
+ */
 class StatusBarHealth extends DrawableObject {
+
+    /**
+     * An array of image paths representing different health levels for the status bar.
+     * The images indicate the health percentage in 20% increments.
+     * @type {string[]}
+     */
     IMAGES_HEALTH = [
         'img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
         'img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
@@ -8,8 +18,15 @@ class StatusBarHealth extends DrawableObject {
         'img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
     ];
 
+    /**
+     * The current health percentage of the player.
+     * @type {number}
+     */
     percentage = 100;
 
+    /**
+     * Constructs a new StatusBarHealth instance.
+     */
     constructor() {
         super();
         this.loadImages(this.IMAGES_HEALTH);
@@ -20,14 +37,22 @@ class StatusBarHealth extends DrawableObject {
         this.width = 220;
     }
 
+    /**
+     * Sets the current health percentage of the player and updates the displayed image.
+     * @param {number} percentage - The current health percentage to set.
+     */
     setPercentage(percentage) {
         this.percentage = percentage;
         let imagePath = this.IMAGES_HEALTH[this.resolveImagesHealthIndex()];
         this.img = this.imageCache[imagePath];
     }
 
+    /**
+     * Resolves the index of the image to use based on the current health percentage.
+     * @returns {number} - The index of the image to use.
+     */
     resolveImagesHealthIndex() {
-        if (this.percentage == 100) {
+        if (this.percentage === 100) {
             return 5;
         } else if (this.percentage > 80) {
             return 4;
