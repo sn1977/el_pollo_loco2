e95159076d3fa05b46aae597fcbb3a4e7ab82632
Sascha Nemeth <sascha.nemeth@me.com>
2024-01-25 15:46:12 +0100

    ad jsdoc comments in keyboard.class.js

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,9 +1,13 @@
+/**
+ * Represents the keyboard input state.
+ */
 class Keyboard {
-    LEFT = false;
-    RIGHT = false;
-    UP = false;
-    DOWN = false;
-    SPACE = false;
-    KEY_D = false;
-    KEY_F = false;
+    // Properties to track key states
+    LEFT = false;   // Left arrow key
+    RIGHT = false;  // Right arrow key
+    UP = false;     // Up arrow key
+    DOWN = false;   // Down arrow key
+    SPACE = false;  // Spacebar key
+    KEY_D = false;  // D key
+    KEY_F = false;  // F key
 }
