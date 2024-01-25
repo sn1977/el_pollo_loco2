af75052f2b1c86c3964734341e336fb25e07ed84
Sascha Nemeth <sascha.nemeth@me.com>
2024-01-25 15:13:21 +0100

    ad jsdoc comments in chicken-small.class.js

--- models/chicken-small.class.js
+++ models/chicken-small.class.js
@@ -1,16 +1,51 @@
+/**
+ * Represents a small chicken enemy in the game.
+ * @extends MovableObject
+ */
 class ChickenSmall extends MovableObject {
+    /**
+     * Y-coordinate of the small chicken.
+     * @type {number}
+     */
     y = 375;
+
+    /**
+     * Height of the small chicken.
+     * @type {number}
+     */
     height = 55;
+
+    /**
+     * Width of the small chicken.
+     * @type {number}
+     */
     width = 55;
+
+    /**
+     * Flag indicating whether the small chicken is dead.
+     * @type {boolean}
+     */
     isDead = false;
 
+    /**
+     * Array of images for the walking animation.
+     * @type {string[]}
+     */
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
         'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
     ];
 
+    /**
+     * Array of images for the dead animation.
+     * @type {string[]}
+     */
     IMAGES_DEAD = ['img/3_enemies_chicken/chicken_small/2_dead/dead.png'];
+
+    /**
+     * Constructs a new ChickenSmall instance with initial settings.
+     */
     constructor() {
         super().loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
@@ -20,11 +55,13 @@ class ChickenSmall extends MovableObject {
         this.animate();
     }
 
+    /**
+     * Initiates the small chicken's animations.
+     */
     animate() {
         setStoppableInterval(() => {
             this.moveLeft();
         }, 1000 / 60);
-        // addInterval(movementIntervalId);
 
         setStoppableInterval(() => {
             if (this.isDead) {
@@ -33,6 +70,5 @@ class ChickenSmall extends MovableObject {
                 this.playAnimation(this.IMAGES_WALKING);
             }
         }, 200);
-        // addInterval(animationIntervalId);
     }
 }
