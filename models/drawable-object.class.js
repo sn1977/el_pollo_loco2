8a35b1b5733856da67f9c9b4adb616ffca88c0ac
Sascha Nemeth <sascha.nemeth@me.com>
2024-01-25 15:29:00 +0100

    ad jsdoc comments in drawable-object.class.js

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -1,11 +1,53 @@
+/**
+ * Represents a drawable object in the game.
+ */
 class DrawableObject {
+    /**
+     * X-coordinate of the drawable object.
+     * @type {number}
+     */
     x = 120;
+
+    /**
+     * Y-coordinate of the drawable object.
+     * @type {number}
+     */
     y = 50;
+
+    /**
+     * Height of the drawable object.
+     * @type {number}
+     */
     height = 150;
+
+    /**
+     * Width of the drawable object.
+     * @type {number}
+     */
     width = 140;
+
+    /**
+     * Image object for the drawable.
+     * @type {Image}
+     */
     img;
+
+    /**
+     * Cache for preloaded images.
+     * @type {Object}
+     */
     imageCache = {};
+
+    /**
+     * Index of the currently displayed image.
+     * @type {number}
+     */
     currentImage = 0;
+
+    /**
+     * Offset for collision detection.
+     * @type {{top: number, left: number, right: number, bottom: number}}
+     */
     offset = {
         top: 0,
         left: 0,
@@ -13,11 +55,19 @@ class DrawableObject {
         bottom: 0
     };
 
+    /**
+     * Loads an image from a given path.
+     * @param {string} path - Path to the image file.
+     */
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+    /**
+     * Preloads a set of images.
+     * @param {string[]} arr - Array of image paths to load.
+     */
     loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
@@ -26,6 +76,10 @@ class DrawableObject {
         })
     }
 
+    /**
+     * Draws the object on the canvas context.
+     * @param {CanvasRenderingContext2D} ctx - The canvas rendering context.
+     */
     draw(ctx) {
         try {
             ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
@@ -35,23 +89,4 @@ class DrawableObject {
         }
 
     }
-
-    // drawFrame(ctx) {
-        // if (this instanceof Chicken || this instanceof Character || this instanceof ChickenSmall || this instanceof Bottle || this instanceof Coin) {
-        //     ctx.beginPath();
-        //     ctx.lineWidth = '5';
-        //     ctx.strokeStyle = 'blue';
-        //     ctx.rect(this.x, this.y, this.width, this.height);
-        //     ctx.stroke();
-        // }
-//         if (this instanceof Chicken || this instanceof Character || this instanceof ChickenSmall) {
-//             ctx.beginPath();
-//             ctx.lineWidth = '3';
-//             ctx.strokeStyle = 'red'; // Andere Farbe für den Offset-Rahmen
-//             ctx.rect(this.x + this.offset.left, this.y + this.offset.top,
-//                 this.width - this.offset.right - this.offset.left,
-//                 this.height - this.offset.bottom - this.offset.top);
-//             ctx.stroke();
-//         }
-//     }
 }
