d9f9c8a8b709e81ef99fbf06499c8d1f8eb256cb
Sascha Nemeth <sascha.nemeth@me.com>
2024-01-25 15:48:03 +0100

    ad jsdoc comments in level.class.js

--- models/level.class.js
+++ models/level.class.js
@@ -1,11 +1,22 @@
+/**
+ * Represents a game level.
+ */
 class Level {
-    enemies;
-    clouds;
-    backgroundObjects;
-    coins;
-    bottles;
-    level_end_x = 4400;
+    enemies;            // Array to store enemy objects
+    clouds;             // Array to store cloud objects
+    backgroundObjects;  // Array to store background objects
+    coins;              // Array to store coin objects
+    bottles;            // Array to store bottle objects
+    level_end_x = 4400; // X-coordinate where the level ends
 
+    /**
+     * Creates a new level with the specified game objects.
+     * @param {array} enemies - An array of enemy objects.
+     * @param {array} clouds - An array of cloud objects.
+     * @param {array} backgroundObjects - An array of background objects.
+     * @param {array} coins - An array of coin objects.
+     * @param {array} bottles - An array of bottle objects.
+     */
     constructor(enemies, clouds, backgroundObjects, coins, bottles) {
         this.enemies = enemies;
         this.clouds = clouds;
