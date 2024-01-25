a455ba78ae66cd0f8a07fbe873c5ff8eace47158
Sascha Nemeth <sascha.nemeth@me.com>
2024-01-25 16:04:40 +0100

    ad jsdoc comments in status-bar-endboss.class.js

--- models/status-bar-endboss.class.js
+++ models/status-bar-endboss.class.js
@@ -1,4 +1,13 @@
+/**
+ * Represents a status bar for displaying the health of the end boss in the game.
+ * @extends DrawableObject
+ */
 class StatusBarEndboss extends DrawableObject {
+    /**
+     * An array of image paths representing different health levels for the status bar.
+     * The images indicate the health percentage in 20% increments.
+     * @type {string[]}
+     */
     IMAGES_STATUSBAR_ENDBOSS = [
         'img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
         'img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
@@ -8,8 +17,15 @@ class StatusBarEndboss extends DrawableObject {
         'img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png'
     ];
 
+    /**
+     * The current health percentage of the end boss.
+     * @type {number}
+     */
     percentage;
 
+    /**
+     * Constructs a new StatusBarEndboss instance.
+     */
     constructor() {
         super();
         this.loadImages(this.IMAGES_STATUSBAR_ENDBOSS);
@@ -21,12 +37,20 @@ class StatusBarEndboss extends DrawableObject {
         this.width = 220;
     }
 
+    /**
+     * Sets the current health percentage of the end boss and updates the displayed image.
+     * @param {number} percentage - The current health percentage to set.
+     */
     setPercentage(percentage) {
         this.percentage = percentage;
         let imagePath = this.IMAGES_STATUSBAR_ENDBOSS[this.resolveImagesHealthIndex()];
         this.img = this.imageCache[imagePath];
     }
 
+    /**
+     * Resolves the index of the image to use based on the current health percentage.
+     * @returns {number} - The index of the image to use.
+     */
     resolveImagesHealthIndex() {
         if (this.percentage === 50) {
             return 5;
